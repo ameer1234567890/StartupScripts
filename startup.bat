851f89b8d6301036bffbf7c9f58be40883ba90a4
Ameer Dawood <ameer1234567890@gmail.com>
2021-09-24 20:10:16 +0500

    Normalization

--- startup.bat
+++ startup.bat
@@ -7,14 +7,14 @@ SET SPATH=K:\StartupScripts\Ameer\
 powershell -Command "Write-EventLog -LogName Application -Source SysAdmin -EntryType Information -Message 'Running startup scripts from %SPATH%' -EventId 1"
 ECHO Running startup scripts from %SPATH%
 
-rem Run all bat (CMD) scripts
-FOR /f %%i IN ('dir /b %SPATH%*.bat') DO (
+REM Run all bat (CMD) scripts
+FOR /F %%i IN ('DIR /B %SPATH%*.bat') DO (
   powershell -Command "Write-EventLog -LogName Application -Source SysAdmin -EntryType Information -Message 'Running %%i' -EventId 1"
   call %SPATH%%%i
 )
 
-rem Run all ps1 (Powershell) scripts
-FOR /f %%i IN ('dir /b %SPATH%*.ps1') DO (
+REM Run all ps1 (Powershell) scripts
+FOR /F %%i IN ('DIR /B %SPATH%*.ps1') DO (
   powershell -Command "Write-EventLog -LogName Application -Source SysAdmin -EntryType Information -Message 'Running %%i' -EventId 1"
   powershell -NoProfile -ExecutionPolicy Bypass -Command %SPATH%\%%i
 )
